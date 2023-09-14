d4b0c59c90487fc1ef5c0be013ab1d08520e900b
Thuan Vu <thuanvh@gmail.com>
2023-09-14 23:17:48 +0700

    update

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from typing import Annotated
-from fastapi import FastAPI, Path, status, HTTPException
+from fastapi import FastAPI, Path, Request, status, HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 from mangum import Mangum
 from book import Book
@@ -12,6 +15,20 @@ app = FastAPI()
 USERS_TABLE = os.environ['USERS_TABLE']
 
 list = []
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    # Get the original 'detail' list of errors
+    details = exc.errors()
+    modified_details = []
+    # Replace 'msg' with 'message' for each error
+    for error in details:
+        modified_details.append(str(error["loc"]) + " " + error["msg"])
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": modified_details}),
+    )
+    
 @app.post("/books",
     status_code=status.HTTP_201_CREATED,
     response_model=Book,
