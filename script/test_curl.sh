922582416afbbc8b5ee3772022f09ca6a7e65d1e
Thuan Vu <thuanvh@gmail.com>
2023-09-17 12:05:19 +0700

    update

--- script/test_curl.sh
+++ script/test_curl.sh
@@ -1,23 +1,20 @@
 #API_URL=https://c4z96jmop3.execute-api.us-east-1.amazonaws.com
-API_URL=http://127.0.0.1:8000
+API_URL=http://127.0.0.1:8000/api
 echo "'$API_URL"/books"'"
 
-# curl -X 'POST' \
-#   "'$API_URL"/books' \
-#   -H 'accept: application/json' \
-#   -H 'Content-Type: application/json' \
-#   -d '{
-#   "id": -1,
-#   "author": 0,
-#   "name": "",
-#   "note": "string",
-#   "serial": ""
-# }'
+curl -X 'POST' \
+  "$API_URL/books" \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": "/books/id1",
+  "author": "/authors/id1",
+  "name": "Hello world",
+  "note": "Good book",
+  "serial": "alk12314"
+}'
 
-
-# curl -X 'GET' \
-#   '"'$API_URL"/books/1"'"' \
-#   -H 'accept: application/json'
 
 curl -X 'GET' \
-  'http://127.0.0.1:8000/books/id1' -H 'accept: application/json'
+   "$API_URL/books/id1" \
+   -H 'accept: application/json'
