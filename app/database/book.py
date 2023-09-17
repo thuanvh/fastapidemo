0ad28fa8ab5f0f36100bc7164f1143d1b1e827c8
Thuan Vu <thuanvh@gmail.com>
2023-09-17 17:44:01 +0700

    update test

--- app/database/book.py
+++ app/database/book.py
@@ -5,23 +5,18 @@ class Book(BaseModel):
         title="Name of the book", 
         min_length=8, max_length=50,
         pattern='/books/.*',
-        required=True
     )
     author: str = Field(
         title="Author of the book",
         min_length=9, max_length=50,
         pattern='/authors/.*',
-        required=True
     )
     name: str = Field(
         title="Name of the book", min_length=1, max_length=300,
-        required=True
     )
     note: str = Field(
         title="Note of the book", max_length=1000,
-        required=True
     )
     serial: str = Field(
         title="Serial of the book", max_length=100,
-        required=True
     )
